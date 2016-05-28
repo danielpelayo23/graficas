e45e4b11538e1f46bf901bc71c7306649ebc71d3
Daniel Pelayo <daniel.pelayo23@gmail.com>
2016-05-27 22:03:41 -0430

    Funcionalidad Lista, faltan controles

--- Proyecto-Olas/PlantillaOpenGL01/main.cpp
+++ Proyecto-Olas/PlantillaOpenGL01/main.cpp
@@ -1,45 +1,44 @@
+// Cubica
 
 #include <GL\glew.h>
 #include <GL\freeglut.h>
 #include <iostream>
-
+#include <cmath>
 using namespace std;
 
 #define DEF_floorGridScale	1.0
 #define DEF_floorGridXSteps	10.0
 #define DEF_floorGridZSteps	10.0
+float PI  = 3.141592654;
+bool activateGrid = false;
 
-GLfloat knots01[8] = {
-	0.0,0.0,0.0,0.0,1.0,1.0,1.0,1.0
-};
-
-GLfloat knots02[9] = {
-	0.0,0.0,0.0,0.0,0.5,1.0,1.0,1.0,1.0
-};
-
-GLfloat ctrlpointsCurveNurbs01[4][3] = {
-	{-10.0,0.0,0.0},{-5.0,0.0,0.0},
-	{5.0,0.0,8.0}, {10.0,0.0,0.0}
-};
-
-GLfloat ctrlpointsCurveNurbs02[5][3] = {
-	{-2.0,2.0,0.0},{-1.0,2.0,2.0},
-	{1.0,2.0,-2.0}, {2.0,2.0,0.0},{2.0,2.0,1.0}
-};
-
-GLfloat ctrPoints01[4][3] = {
-	{-4.0,0.0,0.0},{-2.0,2.0,0.0},{4.0,0.0,0.0},{2.0,2.0,0.0}
-};
-
-GLfloat ctlpointsNurbsSurf[4][4][3];
-GLfloat knotsSurfs[8] = {
-	0.0,0.0,0.0,0.0,1.0,1.0,1.0,1.0
-};
-
-GLUnurbsObj *theNurb01, *theNurb02, *theNurbSurf;
+GLUnurbsObj *theNurb;
+GLfloat ctlpoints[21][21][3];
+GLfloat knots[25];
+//Variables Ecuacion//
+//Ola 1//
+//Usuario controla ola 1 al presionar tecla 1//
+float L1 = 8; //Valor Cambiante cuando tocan a-z
+float A1 = 0; //Valor Cambiante cuando tocan s-x
+float S1 = 2; //Valor Cambiante cuando tocan d-c
+float Dx1 = 0; //Valor Cambiante cuando tocan f-v
+float Dy1 = -1; //Valor Cambiante cuando tocan g-b
+float W1 = 2*PI/L1;
+float Dx1norm = Dx1/sqrt(pow(Dx1,2)+pow(Dy1,2));
+float Dy1norm = Dy1/sqrt(pow(Dx1,2)+pow(Dy1,2));
+//Ola 2//
+//Usuario controla ola 2 al presionar tecla 2//
+float L2 = 3; //Valor Cambiante cuando tocan a-z
+float A2 = 0.3; //Valor Cambiante cuando tocan s-x
+float S2 = 1; //Valor Cambiante cuando tocan d-c
+float Dx2 = 1; //Valor Cambiante cuando tocan f-v
+float Dy2 = 1; //Valor Cambiante cuando tocan g-b
+float Dx2norm = Dx2/sqrt(pow(Dx2,2)+pow(Dy2,2));
+float Dy2norm = Dy2/sqrt(pow(Dx2,2)+pow(Dy2,2));
+float W2 = 2*PI/L2;
 float t;
+
 void ejesCoordenada() {
-	
 	glLineWidth(2.5);
 	glBegin(GL_LINES);
 		glColor3f(1.0,0.0,0.0);
@@ -81,137 +80,120 @@ void ejesCoordenada() {
 void changeViewport(int w, int h) {
 	
 	float aspectratio;
-	aspectratio = (float) w / (float) h;
-	/*
+
 	if (h==0)
 		h=1;
-	if (w<=h)
-		glOrtho(-10,10,-10/aspectratio,10/aspectratio,1.0,-1.0);
-	else
-		glOrtho(-10*aspectratio,10*aspectratio,-10,10,1.0,-1.0);
-	*/
+
 	
    glViewport (0, 0, (GLsizei) w, (GLsizei) h); 
    glMatrixMode (GL_PROJECTION);
    glLoadIdentity ();
-   gluPerspective(30,aspectratio,1.0, 200.0);
-   
+   gluPerspective(30, (GLfloat) w/(GLfloat) h, 1.0, 200.0);
+   glMatrixMode (GL_MODELVIEW);
 
+}
+
+void colorSelect(string color){
+	if (color.compare("red") == 0)
+		glColor3f(1,0,0);
+	else if (color.compare("green") == 0)
+		glColor3f(0,1,0);
+	else if (color.compare("blue") == 0)
+		glColor3f(0,0,1);
+	else if (color.compare("yellow") == 0)
+		glColor3f(1,1,0);
+	else if (color.compare("yellow1") == 0)
+		glColor3f(0.918,0.835,0.078);
+	else if (color.compare("yellow2") == 0)
+		glColor3f(0.659,0.592,0.212);
+	else if (color.compare("grey") == 0)
+		glColor3f(0.72,0.72,0.67);
+	else if (color.compare("orange") == 0)
+		glColor3f(1,0.5,0);
+	else if (color.compare("purple") == 0)
+		glColor3f(1,0,1);
+	else if (color.compare("white") == 0)
+		glColor3f(1,1,1);
+	else if (color.compare("black") == 0) 
+		glColor3f(0,0,0);
+	else if (color.compare("skin") == 0)
+		glColor3f(1,0.84,0.76);
+	else if (color.compare("brown") == 0)
+		glColor3f(0.55,0.27,0.07);
+	else
+		glColor3f(1,0,0);
+}
+void drawPoint(float x,float y,float z, string color){
+	colorSelect(color);
+	glBegin(GL_POINTS);
+		glVertex3f(x,y,z);
+	glEnd();
+}
+
+float productoEscalar(float a[], float b[]){
+	float result = 0;
+	for (int i = 0; i < 2; i++)
+	{
+		result = a[i] * b[i] + result;
+		
+	}
+	return result;
 }
 
 void init_surface() {
-	 ctlpointsNurbsSurf[0][0][0] = 2.0;
-	 ctlpointsNurbsSurf[0][0][1] = 0.0;
-	 ctlpointsNurbsSurf[0][0][2] = 2.0;
+	for (int i = 0; i <21; i++) {
+		for (int j = 0; j < 21; j++) {
+			ctlpoints[i][j][0] = 10 -(GLfloat)j;
+			ctlpoints[i][j][1] = 0.0;
+			ctlpoints[i][j][2] = 10 - (GLfloat)i;
+		}
+	}
 
-	 ctlpointsNurbsSurf[0][1][0] = 1.0;
-	 ctlpointsNurbsSurf[0][1][1] = 0.0;
-	 ctlpointsNurbsSurf[0][1][2] = 2.0;
-
-	 ctlpointsNurbsSurf[0][2][0] = -1.0;
-	 ctlpointsNurbsSurf[0][2][1] = 0.0;
-	 ctlpointsNurbsSurf[0][2][2] = 2.0;
-
-	 ctlpointsNurbsSurf[0][3][0] = -2.0;
-	 ctlpointsNurbsSurf[0][3][1] = 0.0;
-	 ctlpointsNurbsSurf[0][3][2] = 2.0;
-
-	 ctlpointsNurbsSurf[1][0][0] = 2.0;
-	 ctlpointsNurbsSurf[1][0][1] = 0.0;
-	 ctlpointsNurbsSurf[1][0][2] = 1.0;
-
-	 ctlpointsNurbsSurf[1][1][0] = 1.0;
-	 ctlpointsNurbsSurf[1][1][1] = 0.0;
-	 ctlpointsNurbsSurf[1][1][2] = 1.0;
-
-	 ctlpointsNurbsSurf[1][2][0] = -1.0;
-	 ctlpointsNurbsSurf[1][2][1] = 0.0;
-	 ctlpointsNurbsSurf[1][2][2] = 1.0;
-
-	 ctlpointsNurbsSurf[1][3][0] = -2.0;
-	 ctlpointsNurbsSurf[1][3][1] = 0.0;
-	 ctlpointsNurbsSurf[1][3][2] = 1.0;
-
-	 ctlpointsNurbsSurf[2][0][0] = 2.0;
-	 ctlpointsNurbsSurf[2][0][1] = 0.0;
-	 ctlpointsNurbsSurf[2][0][2] = -1.0;
-
-	 ctlpointsNurbsSurf[2][1][0] = 1.0;
-	 ctlpointsNurbsSurf[2][1][1] = 0.0;
-	 ctlpointsNurbsSurf[2][1][2] = -1.0;
-
-	 ctlpointsNurbsSurf[2][2][0] = -1.0;
-	 ctlpointsNurbsSurf[2][2][1] = 0.0;
-	 ctlpointsNurbsSurf[2][2][2] = -1.0;
-
-	 ctlpointsNurbsSurf[2][3][0] = -2.0;
-	 ctlpointsNurbsSurf[2][3][1] = 0.0;
-	 ctlpointsNurbsSurf[2][3][2] = -1.0;
-
-	 ctlpointsNurbsSurf[3][0][0] = 2.0;
-	 ctlpointsNurbsSurf[3][0][1] = 0.0;
-	 ctlpointsNurbsSurf[3][0][2] = -2.0;
-
-	 ctlpointsNurbsSurf[3][1][0] = 1.0;
-	 ctlpointsNurbsSurf[3][1][1] = 0.0;
-	 ctlpointsNurbsSurf[3][1][2] = -2.0;
-
-	 ctlpointsNurbsSurf[3][2][0] = -1.0;
-	 ctlpointsNurbsSurf[3][2][1] = 0.0;
-	 ctlpointsNurbsSurf[3][2][2] = -2.0;
-
-	 ctlpointsNurbsSurf[3][3][0] = -2.0;
-	 ctlpointsNurbsSurf[3][3][1] = 0.0;
-	 ctlpointsNurbsSurf[3][3][2] = -2.0;
-
+	for (int i = 0; i < 25; i++)
+	{
+		if ((0 <= i && i <= 3))
+		{
+			knots[i] = 0.0;
+		}
+		else if ((21 <= i && i <= 24)){
+			knots[i] = 1.0;
+		}
+		else{
+			knots[i] = knots[i-1] + 0.058;
+		}
+	}
+	
+	
+	
 }
+void animacion(int value) {
+	t += 0.1;
 
-void changePoints(int value){
-	t+=0.1;
-	ctlpointsNurbsSurf[1][1][1] =3*sin(t);
-	ctlpointsNurbsSurf[1][2][1] =-3*sin(t);
-	ctlpointsNurbsSurf[2][1][1] =-3*sin(t);
-	ctlpointsNurbsSurf[2][2][1] =3*sin(t);
 
-	glutTimerFunc(10,changePoints,1);
-	glutPostRedisplay();
+	for (int i = 0; i <21; i++) {
+		for (int j = 0; j < 21; j++) {
+			ctlpoints[i][j][1] = (A1 * sin( ((Dx1norm*ctlpoints[i][j][0] + Dy1norm*ctlpoints[i][j][2]) * W1) + t*S1*2*PI/L1)) + (A2 * sin( ((Dx2norm*ctlpoints[i][j][0] + Dy2norm*ctlpoints[i][j][2]) * W2) + t*S2*2*PI/L2));
+		}
+	}
+	glutTimerFunc(10,animacion,0);
+    glutPostRedisplay();	
 }
-
 void init(){
-
-
-
    glEnable(GL_LIGHTING);
    glEnable(GL_LIGHT0);
    glEnable(GL_DEPTH_TEST);
    glEnable(GL_AUTO_NORMAL);
    glEnable(GL_NORMALIZE);
-   theNurb01 = gluNewNurbsRenderer();
-   theNurb02 = gluNewNurbsRenderer();
-   gluNurbsProperty(theNurb01, GLU_SAMPLING_TOLERANCE, 25.0);
-   gluNurbsProperty(theNurb02, GLU_SAMPLING_TOLERANCE, 25.0);
 
-   theNurbSurf = gluNewNurbsRenderer();
-   gluNurbsProperty(theNurbSurf, GLU_SAMPLING_TOLERANCE, 25.0);
-   gluNurbsProperty(theNurbSurf, GLU_DISPLAY_MODE, GLU_FILL);
    init_surface();
 
-   //ctlpointsNurbsSurf[1][1][1] = 3.0;
-   //ctlpointsNurbsSurf[1][2][1] = 3.0;
-   //ctlpointsNurbsSurf[2][1][1] = 3.0;
-   //ctlpointsNurbsSurf[2][2][1] = 3.0;
+   theNurb = gluNewNurbsRenderer();
+   gluNurbsProperty(theNurb, GLU_SAMPLING_TOLERANCE, 15.0);
+   gluNurbsProperty(theNurb, GLU_DISPLAY_MODE, GLU_FILL);
 
-
-   glEnable(GL_DEPTH_TEST);
-   glEnable(GL_AUTO_NORMAL);
-   glEnable(GL_NORMALIZE);
-
-   glutTimerFunc(10,changePoints,1);
-   t=0.0;
-
+   glutTimerFunc(0, animacion, 0);
+   t = 0.0;
 }
-
-
 
 void Keyboard(unsigned char key, int x, int y)
 {
@@ -219,7 +201,13 @@ void Keyboard(unsigned char key, int x, int y)
   {
 	case 27:             
 		exit (0);
-		break;
+	break;
+	case 'g':// usar grid
+		if(activateGrid)
+			activateGrid = false;
+		else
+			activateGrid = true;
+	break;
 	
   }
 }
@@ -233,134 +221,96 @@ void render(){
 	GLfloat zExtent, xExtent, xLocal, zLocal;
     int loopX, loopZ;
 
-	glMatrixMode (GL_MODELVIEW);
 	glLoadIdentity ();                       
-	//gluLookAt (25.0, 12.0, 4.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
-	gluLookAt (10.0, 5.0, 10.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
-
-	const GLfloat pos[]= {10.0,10.0,0.0,0.0};
-	glLightfv(GL_LIGHT0,GL_POSITION,pos);
-	glEnable(GL_LIGHTING);
-	glDisable(GL_LIGHT0);
+	gluLookAt (25.0, 12.0, 4.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
 	
 
 	// Luz y material
-	glEnable(GL_LIGHT0);
-	GLfloat mat_diffuse[] = { 1.0, 0.5, 0.5, 1.0 };
-	GLfloat mat_specular[] = { 1.0, 1.0, 1.0, 1.0 };
-	GLfloat mat_shininess[] = { 100.0 };
+
+	GLfloat mat_diffuse[] = { 0.6, 0.6, 0.9, 1.0 };
+	GLfloat mat_specular[] = { 0.8, 0.8, 1.0, 1.0 };
+	GLfloat mat_shininess[] = { 60.0 };
 	
 	glMaterialfv(GL_FRONT, GL_DIFFUSE, mat_diffuse);
 	glMaterialfv(GL_FRONT, GL_SPECULAR, mat_specular);
 	glMaterialfv(GL_FRONT, GL_SHININESS, mat_shininess);
-
-	gluBeginSurface(theNurbSurf);
-	gluNurbsSurface(theNurbSurf,
-					8,knotsSurfs,8,knotsSurfs,
-					4*3,3,&ctlpointsNurbsSurf[0][0][0],
-					4,4,GL_MAP2_VERTEX_3);
-	gluEndSurface(theNurbSurf);
 	
-	/*
-    GLfloat light_ambient[] = { 1.0, 0.5, 0.5, 1.0 };
+
+    GLfloat light_ambient[] = { 0.0, 0.0, 0.2, 1.0 };
 	GLfloat light_diffuse[] = { 0.8, 0.8, 0.8, 1.0 };
-	GLfloat light_specular[] = { 1.0, 1.0, 1.0, 1.0 };
+	GLfloat light_specular[] = { 0.6, 0.6, 0.6, 1.0 };
 	GLfloat light_position[] = { -10.0, 5.0, 0.0, 1.0 };
 
 	glLightfv(GL_LIGHT0, GL_AMBIENT, light_ambient);
 	glLightfv(GL_LIGHT0, GL_DIFFUSE, light_diffuse);
 	glLightfv(GL_LIGHT0, GL_SPECULAR, light_specular);
 	glLightfv(GL_LIGHT0, GL_POSITION, light_position);   
-	*/
-	//glutSolidSphere(4,30,30);
-
 
 
 	
-	// Render Grid 
-	glDisable(GL_LIGHTING);
-	glLineWidth(1.0);
-	glPushMatrix();
-	glRotatef(90,1.0,0.0,0.0);
-    glColor3f( 0.0, 0.7, 0.7 );
-    glBegin( GL_LINES );
-    zExtent = DEF_floorGridScale * DEF_floorGridZSteps;
-    for(loopX = -DEF_floorGridXSteps; loopX <= DEF_floorGridXSteps; loopX++ )
+	// Render Grid
+	if (activateGrid)
 	{
-	xLocal = DEF_floorGridScale * loopX;
-	glVertex3f( xLocal, -zExtent, 0.0 );
-	glVertex3f( xLocal, zExtent,  0.0 );
-	}
-    xExtent = DEF_floorGridScale * DEF_floorGridXSteps;
-    for(loopZ = -DEF_floorGridZSteps; loopZ <= DEF_floorGridZSteps; loopZ++ )
-	{
-	zLocal = DEF_floorGridScale * loopZ;
-	glVertex3f( -xExtent, zLocal, 0.0 );
-	glVertex3f(  xExtent, zLocal, 0.0 );
-	}
-    glEnd();
-	ejesCoordenada();
-    glPopMatrix();
-	glEnable(GL_LIGHTING);
-	// Fin Grid
-	glColor3f(0.0,1.0,0.0);
-	glPointSize(10.0);
-	glBegin(GL_POINTS);
-		glVertex2f(0.0,0.0);
-	glEnd();
-	glEnable(GL_BLEND);
-	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-	glEnable(GL_LINE_SMOOTH);
-	
-	//Bezier
-	glMap1f(GL_MAP1_VERTEX_3, 0.0,1.0,3,4, &ctrPoints01[0][0]);
-	glEnable(GL_MAP1_VERTEX_3);
-
-	int i;
-	float res = 20.0f; //resolucion de la curva
-	glColor3f(1.0,1.0,0.0);
-
-	glBegin(GL_LINE_STRIP);
-	for (i=0; i<=res; i++) {
-		glEvalCoord1f((GLfloat) i / res);
-	}
-	glEnd();
-
-	//Grafica ptos de control
-
-	glPointSize(5.0);
-	glColor3f(1.0,0.0,1.0);
-	glBegin(GL_POINTS);
-		for (i=0; i<4; i++){
-			glVertex3fv(&ctrPoints01[i][0]);
+		glDisable(GL_LIGHTING);
+		glLineWidth(1.0);
+		glPushMatrix();
+		glRotatef(90,1.0,0.0,0.0);
+		glColor3f( 0.0, 0.7, 0.7 );
+		glBegin( GL_LINES );
+		zExtent = DEF_floorGridScale * DEF_floorGridZSteps;
+		for(loopX = -DEF_floorGridXSteps; loopX <= DEF_floorGridXSteps; loopX++ )
+		{
+		xLocal = DEF_floorGridScale * loopX;
+		glVertex3f( xLocal, -zExtent, 0.0 );
+		glVertex3f( xLocal, zExtent,  0.0 );
 		}
-	glEnd();
-	
+		xExtent = DEF_floorGridScale * DEF_floorGridXSteps;
+		for(loopZ = -DEF_floorGridZSteps; loopZ <= DEF_floorGridZSteps; loopZ++ )
+		{
+		zLocal = DEF_floorGridScale * loopZ;
+		glVertex3f( -xExtent, zLocal, 0.0 );
+		glVertex3f(  xExtent, zLocal, 0.0 );
+		}
+		glEnd();
+		ejesCoordenada();
+		glPopMatrix();
+		glEnable(GL_LIGHTING);
+	}
+	// Fin Grid
+
+
+	//Suaviza las lineas
+	glEnable(GL_BLEND); glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+	glEnable( GL_LINE_SMOOTH );	
+
 	glPushMatrix();
+		gluBeginSurface(theNurb);    
+			gluNurbsSurface(theNurb, 
+						   25, knots, 25, knots,
+						   21 * 3, 3, &ctlpoints[0][0][0], 
+						   4, 4, GL_MAP2_VERTEX_3);
+		gluEndSurface(theNurb);	
+	glPopMatrix();
+	
+	/* Muestra los puntos de control */
 
-	//CurvaNurbs
-	glColor3f(0.0,1.0,0.0);
-	gluBeginCurve(theNurb01);
-		gluNurbsCurve(theNurb01,8,knots01,3,&ctrlpointsCurveNurbs01[0][0],4,GL_MAP1_VERTEX_3);
-	gluEndCurve(theNurb01);
-
-	gluBeginCurve(theNurb02);
-		gluNurbsCurve(theNurb02,8,knots02,3,&ctrlpointsCurveNurbs02[0][0],4,GL_MAP1_VERTEX_3);
-	gluEndCurve(theNurb02);
-
-	glPopMatrix();		
-
+	/*
+		int i,j;
+		glPointSize(5.0);
+		glDisable(GL_LIGHTING);
+		colorSelect("yellow");
+		glBegin(GL_POINTS);
+		for (i = 0; i <21; i++) {
+			for (j = 0; j < 21; j++) {
+	            glVertex3f(ctlpoints[i][j][0], 	ctlpoints[i][j][1], ctlpoints[i][j][2]);
+			}
+		}
+		glEnd();
+		glEnable(GL_LIGHTING);
+	*/
 	glDisable(GL_BLEND);
 	glDisable(GL_LINE_SMOOTH);
-
 	glutSwapBuffers();
-}
-
-void animacion(int value) {
-	
-	glutTimerFunc(10,animacion,1);
-    glutPostRedisplay();
-	
 }
 
 int main (int argc, char** argv) {
@@ -373,7 +323,7 @@ int main (int argc, char** argv) {
 
 	glutCreateWindow("Nurbs Proyecto - Ola");
 
-	init ();
+	init();
 
 	glutReshapeFunc(changeViewport);
 	glutDisplayFunc(render);
